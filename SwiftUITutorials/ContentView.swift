f844c34e1229e8a4292e251e8990ec5c261343ba
csson <csson@cozmogames.com>
2022-06-16 13:21:12 +0900

    Section1 Create a New Project and Explore the Canvas
    step1 ~ 6

---
+++ SwiftUITutorials/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITutorials
+//
+//  Created by csson on 2022/06/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
