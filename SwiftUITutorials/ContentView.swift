02860ca450169f0ff9eb10781d9a635f1d7292bc
SonCS <hp3943@hanmail.net>
2022-06-16 13:38:35 +0900

    Section2 Customize the Text View

--- SwiftUITutorials/ContentView.swift
+++ SwiftUITutorials/ContentView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, SwiftUI!!")
+        Text("Turtle Rock")
+            .font(.title)
+        // Step6 remvoing foregroundColor
+//            .foregroundColor(Color.green)
             .padding()
     }
 }
